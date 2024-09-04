f49c3996fa47c86bf01712d0673eb9241666eee5
Pratik Anand <pratikanand0512@gmail.com>
2024-09-04 15:29:54 +0530

    default color value changed

--- backgroundChanger/src/App.jsx
+++ backgroundChanger/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 
 function App() {
-  const [color,setColor] = useState("olive")
+  const [color,setColor] = useState("black")
 
   return (
     <div className="w-screen h-screen duration-200" 
